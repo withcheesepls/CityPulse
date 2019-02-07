245ae26ffa9d0bdb8ea6e4bc56c0363f43c40af5
withcheesepls <josue.rojas.rojas@gmail.com>
2019-02-06 22:08:19 -0500

    added failures...

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ $(document).ready(function(){
     let boxWidth = $box.width();
     let points = makePoints(boxHeight, boxWidth, 15);
     drawPoints(points, $box);
-    let linePoints = makeLinePoints(boxHeight, boxWidth, 7, 10);
-    drawLines(linePoints, $box);
+    let linePoints = makeLinePoints(boxHeight, boxWidth, 5, 7, 10);
+    drawLines(linePoints, $box, 'line-secondary');
+    let mainLine = makeLinePoints(boxHeight, boxWidth, 1, 7, 15);
+    drawLines(mainLine, $box, 'line-main');
   }
 
-  function drawLines(linePoints, $box){
+  function drawLines(linePoints, $box, lineClass){
     for(let ps in linePoints){
-      console.table(linePoints[ps]);
       prevPoint = [];
+      let path = $('<div class="path"></div>');
       for(let p in linePoints[ps]){
         let line = '';
         if(linePoints[ps][p][2] == 'beginning'){
@@ -27,67 +29,86 @@ $(document).ready(function(){
         }
         else if(linePoints[ps][p][2] == 'up'){
           let height = prevPoint[0] - linePoints[ps][p][0];
-          line = $(`<div class="line line-secondary up" style="width:1px; height: ${height}px"></div>`)
+          line = $(`<div class="line ${lineClass} up" style="width:1px; height: ${height}px"></div>`)
           line.offset({top: linePoints[ps][p][0] , left: linePoints[ps][p][1]});
         }
         else if(linePoints[ps][p][2] == 'right'){
           let width = linePoints[ps][p][1] - prevPoint[1];
-          line = $(`<div class="line line-secondary right" style="width:${width}px; height: 1px"></div>`)
+          line = $(`<div class="line ${lineClass} right" style="width:${width}px; height: 1px"></div>`)
           line.offset({top: prevPoint[0], left: prevPoint[1]});
         }
         else if(linePoints[ps][p][2] == 'down'){
           let height = linePoints[ps][p][0] - prevPoint[0];
-          line = $(`<div class="line line-secondary down" style="width:1px; height: ${height}px"></div>`)
+          line = $(`<div class="line ${lineClass} down" style="width:1px; height: ${height}px"></div>`)
           line.offset({top: prevPoint[0], left: prevPoint[1]});
         }
         else if(linePoints[ps][p][2] == 'left'){
           let width = prevPoint[1] - linePoints[ps][p][1];
-          line = $(`<div class="line line-secondary left" style="width:${width}px; height: 1px"></div>`)
+          line = $(`<div class="line ${lineClass} left" style="width:${width}px; height: 1px"></div>`)
           line.offset({top: linePoints[ps][p][0], left: linePoints[ps][p][1]});
         }
         prevPoint = linePoints[ps][p];
-        let path = $('<div class="path"></div>');
         path.append(line);
-        $box.append(path);
       }
+      $box.append(path);
     }
   }
 
   // this function returns an array of array points represnting the lines
-  function makeLinePoints(height, width, amount, amountMaxTurns){
+  function makeLinePoints(height, width, amount, amountMinTurns, amountMaxTurns){
     let linePoints = [];
     for(let i = 0; i < amount; i++){
-      let amountLines = getRandomInt(1, amountMaxTurns);
+      let amountLines = getRandomInt(amountMinTurns, amountMaxTurns);
       let singlePath = [];
       let prevPoint = null;
       let prevDir = null;
       for(let j = 0; j < amountLines; j++){
         let turnProb = Math.random();
         let position = [];
+        let change = false;
+        let mainProb = .5;
+        let upProb = .5;
+        let leftProb = .5;
         // for the lines to be straight they must turn left, right, up, or down.(and not back)
         if(!prevDir) {
           position = [getRandomArbitrary(0, height), getRandomArbitrary(0, width), 'beginning'];
           prevDir = 'beginning';
+          change=true;
         }
-        else if(prevDir != 'up' && turnProb < .25){
-          console.log('random up', prevPoint[0]);
-          position = [getRandomArbitrary(0, prevPoint[0]), prevPoint[1], 'up'];
-          prevDir = 'up';
+        else if(turnProb < mainProb && (prevDir != 'up' && prevDir != 'down')){
+          if(Math.random() < upProb && prevDir != 'up'){
+            position = [getRandomArbitrary(0, prevPoint[0]), prevPoint[1], 'up'];
+            prevDir = 'up';
+            change=true;
+            upProb =.7;
+          }
+          else if(prevDir != 'down'){
+            position = [getRandomArbitrary(prevPoint[0], height), prevPoint[1], 'down'];
+            prevDir = 'down';
+            change=true;
+            upProb = .2;
+          }
+          // mainProb = .7;
         }
-        else if(prevDir != 'right' && turnProb < .5){
-          position = [prevPoint[0], getRandomArbitrary(prevPoint[1], width), 'right'];
-          prevDir = 'right';
+        else if(prevDir != 'left' && prevDir != 'right'){
+          if(Math.random() < leftProb && prevDir != 'left'){
+            position = [prevPoint[0], getRandomArbitrary(0, prevPoint[1]), 'left'];
+            prevDir = 'left';
+            change=true;
+            leftProb = .7;
+          }
+          else if(prevDir != 'right'){
+            position = [prevPoint[0], getRandomArbitrary(prevPoint[1], width), 'right'];
+            prevDir = 'right';
+            change=true;
+            leftProb = .2;
+          }
         }
-        else if(prevDir != 'down' && turnProb < .75){
-          position = [getRandomArbitrary(prevPoint[0], height), prevPoint[1], 'down'];
-          prevDir = 'down';
+        if(change){
+          prevPoint = position;
+          singlePath.push(position);
         }
-        else if(prevDir != 'left' && turnProb < 1){
-          position = [prevPoint[0], getRandomArbitrary(0, prevPoint[1]), 'left'];
-          prevDir = 'left';
-        }
-        prevPoint = position;
-        singlePath.push(position);
+        else j-=1;
       }
       linePoints.push(singlePath);
     }
